8800088d3009702bd50d90d7c79765f71a65c8c0
kmurabayashi <imada.nigero@gmail.com>
2022-05-16 00:14:17 +0900

    柴犬も秋田犬も選択できるようになりました。可愛いですね。

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { fetchImages } from "./api";
 function Header() {
     return (
         <header className="hero is-dark is-bold">
@@ -10,37 +12,90 @@ function Header() {
     );
 }
 
-function Image() {
+function Image(props) {
     return (
         <div className="card">
             <div className="card-image">
                 <figure className="image">
-                    <img
-                        src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
-                        alt="cute dog!"
-                    />
+                    <img src={props.src} alt="cute dog!" />
                 </figure>
             </div>
         </div>
     );
 }
+function Loading() {
+    return <p>Loading...</p>;
+}
 
-function Gallery() {
+function Gallery(props) {
+    const { urls } = props;
+    if (urls == null) {
+        return <Loading />;
+    }
     return (
         <div className="columns is-vcentered is-multiline">
-            <div className="column is-3">
-                <Image />
-            </div>
+            {urls.map((url) => {
+                return (
+                    <div key={url} className="column is-3">
+                        <Image src={url}/>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+
+function Form(props) {
+    function handleSubmit(event) {
+        event.preventDefault();
+        const { breed } = event.target.elements;
+        props.onFormSubmit(breed.value);
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="field has-addons">
+                    <div className="control is-expanded">
+                        <div className="select is-fullwidth">
+                            <select name="breed" defaultValue="shiba">
+                                <option value="shiba">Shiba</option>
+                                <option value="akita">Akita</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="control">
+                        <button type="submit" className="button is-dark">
+                            Reload
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 }
 
 function Main() {
+    const [urls, setUrls] = useState(null);
+    useEffect(() => {
+        fetchImages("shiba").then((urls) => {
+            setUrls(urls);
+        });
+    }, []);
+    function reloadImages(breed) {
+        fetchImages(breed).then((urls) => {
+            setUrls(urls);
+        });
+    }
     return (
         <main>
             <section className="section">
                 <div className="container">
-                    <Gallery />
+                    <Form onFormSubmit={reloadImages} />
+                </div>
+            </section>
+            <section className="section">
+                <div className="container">
+                    <Gallery urls={urls} />
                 </div>
             </section>
         </main>
